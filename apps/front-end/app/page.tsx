7818c5f1a9edb50e88463db522d5bbcc07a07461
dswvgw6412 <dswvgw6412@naver.com>
2025-11-09 15:15:13 +0900

    chore(frontend): Remove unnecessary code

--- apps/front-end/app/page.tsx
+++ apps/front-end/app/page.tsx
@@ -1,14 +1,3 @@
-'use client';
-
-import { useLogin } from '@/hooks/useLogin';
-
 export default function Home() {
-  const { handleGithubLogin } = useLogin();
-
-  return (
-    <div>
-      <h1 className='bg-blue-50'>야구 게임</h1>
-      <button onClick={handleGithubLogin}>깃허브 로그인 테스트</button>
-    </div>
-  );
+  return <div></div>;
 }
