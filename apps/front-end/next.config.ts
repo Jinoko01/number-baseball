c7ebb42e9a9be896da62d33cb8da4c90a0be5a6f
dswvgw6412 <dswvgw6412@naver.com>
2025-11-09 16:01:54 +0900

    fix(header): Approve github image link

--- apps/front-end/next.config.ts
+++ apps/front-end/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['ui'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- packages/ui/components/Header.tsx
+++ packages/ui/components/Header.tsx
@@ -6,17 +6,20 @@ import Logo from '../assets/logo.webp';
 import defaultProfile from '../assets/defaultProfile.webp';
 
 export function Header() {
-  const { avatar, name } = useAuthStore();
+  const { avatar, id } = useAuthStore();
 
   return (
-    <header className='w-full flex justify-between items-center px-8 py-4 shadow-lg sticky top-0 bg-white'>
-      <div className='flex items-center gap-4'>
+    <header className='w-full flex justify-between items-center px-8 py-2 shadow-lg sticky top-0 bg-white'>
+      <div className='items-center gap-4 hidden sm:flex'>
         <Image src={Logo} alt='로고' width={60} height={60} />
         <h1 className='text-2xl font-bold'>숫자 야구 게임</h1>
       </div>
       <div className='flex items-center gap-2'>
-        <Image src={avatar ?? defaultProfile} alt='profileImage' width={60} height={60} />
-        <p className='font-semibold text-lg'>{name ?? 'Guest'}</p>
+        <div className='rounded-[50%] overflow-hidden border-2 border-black'>
+          <Image src={avatar ?? defaultProfile} alt='profileImage' width={40} height={40} />
+        </div>
+
+        <p className='font-semibold text-lg'>{id ?? 'Guest'}</p>
       </div>
     </header>
   );
