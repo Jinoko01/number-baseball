0e6adc069fba7ef7ac2f55dba304d39b73eb3003
dswvgw6412 <dswvgw6412@naver.com>
2025-11-18 14:21:53 +0900

    chore: chat module

--- apps/back-end/src/modules/chat/chat.module.ts
+++ apps/back-end/src/modules/chat/chat.module.ts
@@ -3,9 +3,17 @@ import { ChatGateway } from './chat.gateway';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { RoomService } from '../room/room.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Room } from '../entities/room.entity';
+import { RoomParticipant } from '../entities/room-participant.entity';
+import { Users } from '../entities/users.entity';
 
 @Module({
-  imports: [ConfigModule, JwtModule.register({})],
+  imports: [
+    ConfigModule,
+    JwtModule.register({}),
+    TypeOrmModule.forFeature([Room, Users, RoomParticipant]),
+  ],
   providers: [ChatGateway, RoomService],
 })
 export class ChatModule {}
